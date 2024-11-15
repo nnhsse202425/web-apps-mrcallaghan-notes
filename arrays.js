f46e0f5eaf56a1f030de49670cecfa25ba306bdd
Matt Callaghan <mcallaghan@naperville203.org>
2024-11-15 10:26:04 -0600

    started array notes in js

--- arrays.js
+++ arrays.js
@@ -3,3 +3,29 @@
 /**********************************************************
  * Arrays
  **********************************************************/
+
+// create an empty array
+let fruits = [];
+
+// create an array with elements
+fruits = ["apple", "banana", "cherry"];
+
+// arrays are zero-indexed
+console.log(fruits[1]); // banana
+
+// use .length to get the number of elements
+console.log(fruits.length); // 3
+
+// elements can be added to arrays (unlike Java)
+fruits[3] = "date";
+console.log(fruits);
+console.log(fruits.length); //  length 4
+
+// elements can be of any type
+const arr = [1, "two", true, { name: "Callaghan" }, fruits];
+console.log(arr);
+
+fruits[11] = "fig";
+console.log(fruits);
+console.log(fruits[7]); // undefined
+console.log(fruits.length); // 12
