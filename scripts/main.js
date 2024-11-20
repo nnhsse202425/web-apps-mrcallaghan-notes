7cbeedd4029a3c7bd1fd1a2bdefddc6ec1e7089f
Matt Callaghan <mcallaghan@naperville203.org>
2024-11-20 08:21:30 -0600

    add client-side js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
 // client-side JavaScript to handle events in the browser
+
+// the querySelector function returns the first element in the document
+//  that matches the specified selector (e.g., h1)
+const myHeading = document.querySelector("h1");
+console.log(typeof myHeading);
+
+// Javascript can modify an attribute of an HTML element
+myHeading.textContent = "Hello World!";
+
+// the querySelector method can also be used to select an element
+//  by its class
+const button = document.querySelector("button.demo");
+
+// the addEventListener function attaches an event handler (i.e., function)
+//  to the specified element associated with the specified event
+button.addEventListener("click", () => {
+  alert("Ouch! Stop poking me!");
+});
+
+// client-side Javascript can modify the attributes of HTML elements in
+//  response to events
+const myImage = document.querySelector("img");
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/SE-GitHub-image.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
