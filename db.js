2f1817a22ae7cbe298cbac342aca9842aa781988
mrcallaghan <mcallaghan@naperville203.org>
2024-11-20 21:47:08 -0600

    test

--- db.js
+++ db.js
@@ -3,3 +3,11 @@
 /**********************************************************
  * MongoDB
  **********************************************************/
+
+const { MongoClient } = require("mongodb");
+
+// load environment variables from a .env file into process.env
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+const client = new MongoClient(process.env.MONGO_URI);
